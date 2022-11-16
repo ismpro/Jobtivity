2fbb0e480cf0ac9851e8ca8c50866fd35804b4e3
ismpro <38300904+ismpro@users.noreply.github.com>
2022-11-16 16:08:59 +0000

    react delete and other configuration

--- app.js
+++ app.js
@@ -25,34 +25,6 @@ console.log(chalk.green(`  Node Mode: ${(global.NODE_DEV ? 'DEV' : 'PRD')}`));
 
 console.log(chalk.green('  Configurating Server'));
 
-//Disabling things for security
-app.use(helmet())
-app.disable('x-powered-by');
-
-/* 
-//Webpack configuration
-if(global.NODE_DEV) {
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const config = require('./webpack.config.js');
-const compiler = webpack(config);
-
-app.use((req, res, next) => {
-  if (['/jobs', '/about', '/jobs/', '/about/', '/home', '/home/', '/admin', '/admin/'].some((url => req.url === url))
-    && !req.url.includes('.png')) {
-    req.url = '/' // this would make express-js serve index.html
-  }
-  next()
-})
-
-app.use(webpackDevMiddleware(compiler));
-app.use(webpackHotMiddleware(compiler, {
-  log: console.log
-}));
-}
-*/
-
 //Logger
 app.use(logger)
 
