de651c79db79a87fa84a15497ad63961010f1d14
ismpro <38300904+ismpro@users.noreply.github.com>
2023-01-02 16:21:49 +0000

    Login started

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const fs = require('fs');
 const chalk = require('chalk');
 const logger = require('./config/logger');
 let DB = require('./config/connection');
-const { isGeneratorFunction } = require('util/types');
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
 
 console.clear()
 console.log(chalk.green('\n  Starting server'));
@@ -33,46 +34,55 @@ let db = new DB({
   schema: 'pw'
 })
 
-//Logger
-app.use(logger)
-
-//Serving statics files
-app.use(express.static(path.join(__dirname, 'www')))
-
-// parse application/json
-app.use(bodyParser.json())
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
-
-//Adding Routes
-app.get('/*', function (req, res, next) {
-  if(fs.existsSync(path.join(global.appRoot, 'www', `${req.path.slice(1)}.html`))) {
-    res.status(200).sendFile(path.join(global.appRoot, 'www', `${req.path.slice(1)}.html`));
-  } else {
-    next();
-  }
-});
-
-app.use('/api', require('./routes/api'));
-app.use('/auth', require('./routes/auth'));
-
-console.log(chalk.green('  Done configurating Server'));
-
 db.connect().then(function () {
+
+  var sessionStore = new MySQLStore({}, DB.pool);
+
+  app.use(session({
+    name: 'sid',
+    resave: true,
+    saveUninitialized: false,
+    secret: process.env.SECRET || 'secretstring',
+    store: sessionStore,
+    cookie: {
+      maxAge: 3600000,
+      sameSite: 'lax',
+      secure: false
+    }
+  }));
+
+  //Logger
+  app.use(logger)
+
+  //Serving statics files
+  app.use(express.static(path.join(__dirname, 'www')))
+
+  // parse application/json
+  app.use(bodyParser.json())
+
+  // parse application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({
+    extended: false
+  }))
+
+  //Adding Routes
+  app.get('/*', function (req, res, next) {
+    if (fs.existsSync(path.join(global.appRoot, 'www', `${req.path.slice(1)}.html`))) {
+      res.status(200).sendFile(path.join(global.appRoot, 'www', `${req.path.slice(1)}.html`));
+    } else {
+      next();
+    }
+  });
+
+  app.use('/api', require('./routes/api'));
+  app.use('/auth', require('./routes/auth'));
+
+  console.log(chalk.green('  Done configurating Server'));
+
   app.listen(app.get("port"),
     () => {
       console.log(chalk.green(`\n  Server Listing on ${app.get("port")}`))
     })
-
-
-    /* let User = require("./models/UserModel");
-
-    let user = new User({id: 1})
-    user.exists().then((res)=> console.log(res)) */
-
 })
 
 
